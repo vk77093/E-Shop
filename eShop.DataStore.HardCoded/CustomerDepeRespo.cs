e58d49e1eff55191409ce3420288d37c7ee93717
vijay kumar <vk77093@gmail.com>
2021-11-09 10:40:59 +0530

    added the dependency injection demo

---
+++ eShop.DataStore.HardCoded/CustomerDepeRespo.cs
@@ -0,0 +1,26 @@
+﻿using eshop.CoreBussiness.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.DataStore.HardCoded
+{
+  public  class CustomerDepeRespo
+    {
+        private List<CustomerDepen> customers;
+        public CustomerDepeRespo()
+        {
+            customers = new List<CustomerDepen>()
+            {
+                new CustomerDepen{Id=101,CustName="vijay",CustAddress="some address bar"},
+                new CustomerDepen{Id=102,CustName="Krishna",CustAddress="some2 address2 bar2"}
+            };
+        }
+        public CustomerDepen GetCustomer(int id)
+        {
+            return customers.FirstOrDefault(c => c.Id == id);
+        }
+    }
+}
---
+++ eshop.CoreBussiness/Models/CustomerDepen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eshop.CoreBussiness.Models
+{
+   public class CustomerDepen
+    {
+        public int Id { get; set; }
+        public string CustName { get; set; }
+        public string CustAddress { get; set; }
+    }
+}
