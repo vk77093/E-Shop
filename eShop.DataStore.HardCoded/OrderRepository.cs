5cf6e28a5302cc80071276a4da0a2ae488cd38fb
vijay kumar <vk77093@gmail.com>
2021-11-11 11:59:39 +0530

    lec 58 OrderRepo added

---
+++ eShop.DataStore.HardCoded/OrderRepository.cs
@@ -0,0 +1,73 @@
+﻿using eshop.CoreBussiness.Models;
+using eShop.UseCases.PluginInterface.DataStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.DataStore.HardCoded
+{
+    public class OrderRepository : IOrderRespository
+    {
+        private Dictionary<int, Order> DicOrders;
+        public OrderRepository()
+        {
+            DicOrders = new Dictionary<int, Order>();
+        }
+        public int CreateOrder(Order order)
+        {
+            order.OrderId = DicOrders.Count + 1;
+            order.UniqueId = Guid.NewGuid().ToString();
+            DicOrders.Add(order.OrderId.Value, order);
+            return order.OrderId.Value;
+        }
+
+        public IEnumerable<Order> GetLineItemsByOrderId(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetOrder(int id)
+        {
+            return DicOrders[id];
+        }
+
+        public Order GetOrderByUniqueId(string uniqueId)
+        {
+            foreach(var order in DicOrders)
+            {
+                if (order.Value.UniqueId == uniqueId) return order.Value;
+
+                return null;
+            }
+            return null;
+        }
+
+        public IEnumerable<Order> GetOrders()
+        {
+            return DicOrders.Values;
+        }
+
+        public IEnumerable<Order> GetOustandingOrders()
+        {
+            var allorders = (IEnumerable<Order>)DicOrders.Values;
+            return allorders.Where(x => x.DateProcessed.HasValue == false);
+        }
+
+        public IEnumerable<Order> GetProcessedOrders()
+        {
+            var allorders = (IEnumerable<Order>)DicOrders.Values;
+            return allorders.Where(x => x.DateProcessed.HasValue);
+        }
+
+        public void UpdateOrder(Order order)
+        {
+            if (order == null || !order.OrderId.HasValue)
+                return;
+            var ord = DicOrders[order.OrderId.Value];
+            if (ord == null) return;
+            DicOrders[order.OrderId.Value] = order;
+        }
+    }
+}
---
+++ eShop.UseCases/PluginInterface/DataStore/IOrderRespository.cs
@@ -0,0 +1,21 @@
+﻿using eshop.CoreBussiness.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.UseCases.PluginInterface.DataStore
+{
+  public  interface IOrderRespository
+    {
+        Order GetOrder(int id);
+        Order GetOrderByUniqueId(string uniqueId);
+        int CreateOrder(Order order);
+        void UpdateOrder(Order order);
+        IEnumerable<Order> GetOrders();
+        IEnumerable<Order> GetOustandingOrders();
+        IEnumerable<Order> GetProcessedOrders();
+        IEnumerable<Order> GetLineItemsByOrderId(int orderId);
+    }
+}
