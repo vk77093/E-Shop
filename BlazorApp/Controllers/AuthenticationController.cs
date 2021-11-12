476fc23aa67c64ce25d22a65fb6bbc896d76cac9
vijay kumar <vk77093@gmail.com>
2021-11-12 17:38:01 +0530

    lec 66 with Login window added

---
+++ BlazorApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,37 @@
+﻿using eShop.DataStore.HardCoded;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [Route("/authenticate")]
+        public async Task<IActionResult> Authenticate([FromQuery] string userName,[FromQuery] string pwd)
+        {
+            if(userName =="admin" && pwd == "admin123")
+            {
+                var userClaims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Name,userName),
+                    new Claim(ClaimTypes.Email,"admin@gmail.com"),
+                    new Claim(ClaimTypes.HomePhone,"1234567891"),
+                };
+                var userIdentity = new ClaimsIdentity(userClaims, CookieCommon.CookieNameStatic);
+                var userPrincipal = new ClaimsPrincipal(userIdentity);
+                await HttpContext.SignInAsync(CookieCommon.CookieNameStatic, userPrincipal);
+            }
+            return Redirect("/outstandingorders");
+        }
+        [Route("/logout")]
+        public async Task<IActionResult> Logout() {
+            await HttpContext.SignOutAsync();
+            return Redirect("/outstandingorders");
+        }
+    }
+}
